109c5519eee51f028ef57f541bf1c643b3d6ab7a
Vigneshwar <75877958+vignu10@users.noreply.github.com>
2021-08-25 17:27:20 +0530

    Create AlphabeticalInversePostioning.c

---
+++ AlphabeticalInversePostioning.c
@@ -0,0 +1,44 @@
+// Consider a character ch.... You must change the characters position to its inverse value...Vice versa
+
+// Example 1:
+//       Input :
+//              a
+//       Output:
+//              1
+
+
+
+// Example 2
+
+// Input:
+//      a
+// output
+//      26
+
+#include<stdio.h>
+int main()
+{
+  char ch;
+  scanf("%c",&ch);
+  printf("%d",'a'-ch);
+  printf("%d",'z'-ch+1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
